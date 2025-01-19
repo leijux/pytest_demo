a83428d818777ee931466b09a9db71933855f42d
leiju <leijuxx@outlook.com>
2025-01-19 13:48:00 +0800

    还原回来异常测试分离

--- testcases/api_test/user/test_01_user_info.py
+++ testcases/api_test/user/test_01_user_info.py
@@ -8,15 +8,24 @@ from utils.asserts import assert_result
 @allure.severity(allure.severity_level.NORMAL)
 @allure.epic("单接口测试")
 @allure.feature("用户模块")
-@allure.story("获取当前用户信息")
+@allure.story("/api/v1/user")
 @allure.link("https://gitea.com/api/swagger#/user/userGetCurrent", name="to swagger")
 @pytest.mark.single
 @pytest.mark.asyncio(loop_scope="module")
 class TestUserInfo:
 
-    @allure.title("获取当前登录用户信息 {param_id}")
+    @allure.title("获取用户信息 {param_id}")
     @allure.description("验证获取当前登录的用户信息接口")
     async def test_user_info(self, user_opn, snapshot, test_data):
+        result = await user_opn.get_user_info(base64.b64encode(
+            f"{test_data.username}:{test_data.password}".encode()).decode())
+
+        assert_result(result, test_data, snapshot)
+
+    @allure.title("获取用户信息 {param_id}")
+    @allure.description("验证获取当前登录的用户信息接口异常basic_auth")
+    @pytest.mark.negative
+    async def test_user_info_with_basic_auth(self, user_opn, snapshot, test_data):
         basic_auth = base64.b64encode(
             f"{test_data.username}:{test_data.password}".encode()).decode() if test_data.basic_auth is None else test_data.basic_auth
 
