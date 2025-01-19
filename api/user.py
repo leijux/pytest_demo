e52490a1c9a6581d85cd501d18e8b170ff93fe82
leiju <leijuxx@outlook.com>
2025-01-19 13:47:16 +0800

    增加用户变量相关接口

--- api/user.py
+++ api/user.py
@@ -5,9 +5,23 @@ from core import RestClient
 
 
 class User(RestClient):
+    prefix: str = "/api/v1"
 
     def __init__(self, http_client: httpx.AsyncClient):
         super().__init__(http_client)
 
-    async def user_info(self, **kwargs) -> requests.Response:
-        return await self.get("/api/v1/user", **kwargs)
+    async def user_info(self, **kwargs) -> httpx.Response:
+        return await self.get(f"{self.prefix}/user", **kwargs)
+
+    async def user_variable(self, variable_name, **kwargs) -> httpx.Response:
+        method = kwargs.pop("method")
+        path = f"{self.prefix}/user/actions/variables/{variable_name}"
+
+        if method == "post":
+            return await self.post(path, **kwargs)
+        elif method == "DELETE":
+            return await self.delete(path, **kwargs)
+        elif method == "PUT":
+            return await self.put(path, **kwargs)
+        else:
+            return await self.get(path, **kwargs)
--- operation/user.py
+++ operation/user.py
@@ -1,11 +1,8 @@
 import logging
-import multiprocessing
 
 import allure
 import httpx
-import requests
 
-import utils
 from core import ResultBase
 from api import User
 
@@ -17,11 +14,13 @@ class UserOpn:
         self.api = User(http_client)
 
     @allure.step("获取用户信息")
-    async def get_user_info(self, basic_auth: str) -> ResultBase:
+    async def get_user_info(self, basic_auth: str = None) -> ResultBase:
         result = ResultBase()
-        header = {
-            "authorization": f"Basic {basic_auth}"
-        }
+
+        header = {}
+
+        if basic_auth:
+            header["authorization"] = f"Basic {basic_auth}"
 
         resp = await self.api.user_info(headers=header)
 
@@ -34,102 +33,96 @@ class UserOpn:
             result.success = True
 
         result.response = resp
+        logger.debug(f"resp text {resp.text}")
         return result
 
-    # def get_all_user_info(self):
-    #     """
-    #     获取全部用户信息
-    #     :return: 自定义的关键字返回结果 result
-    #     """
-    #     result = ResultBase()
-    #     res = self.api.list_all_users()
-    #
-    #     if res.json()["code"] == 0:
-    #         result.success = True
-    #     else:
-    #         result.error = "接口返回码是 【 {} 】, 返回信息：{} ".format(res.json()["code"], res.json()["msg"])
-    #     result.msg = res.json()["msg"]
-    #     result.response = res
-    #     return result
-    #
-    # def get_one_user_info(self, username):
-    #     """
-    #     获取单个用户信息
-    #     :param username:  用户名
-    #     :return: 自定义的关键字返回结果 result
-    #     """
-    #     result = ResultBase()
-    #     res = self.api.list_one_user(username)
-    #     if res.json()["code"] == 0:
-    #         result.success = True
-    #     else:
-    #         result.error = "查询用户 ==>> 接口返回码是 【 {} 】, 返回信息：{} ".format(res.json()["code"],
-    #                                                                                 res.json()["msg"])
-    #     result.msg = res.json()["msg"]
-    #     result.response = res
-    #     logger.info("查看单个用户 ==>> 返回结果 ==>> {}".format(result.response.text))
-    #     return result
-    #
-    # def register_user(self, username, password, telephone, sex="", address=""):
-    #     """
-    #     注册用户信息
-    #     :param username: 用户名
-    #     :param password: 密码
-    #     :param telephone: 手机号
-    #     :param sex: 性别
-    #     :param address: 联系地址
-    #     :return: 自定义的关键字返回结果 result
-    #     """
-    #     result = ResultBase()
-    #     json_data = {
-    #         "username": username,
-    #         "password": password,
-    #         "sex": sex,
-    #         "telephone": telephone,
-    #         "address": address
-    #     }
-    #     header = {
-    #         "Content-Type": "application/json"
-    #     }
-    #     res = self.api.register(json=json_data, headers=header)
-    #     result.success = False
-    #     if res.json()["code"] == 0:
-    #         result.success = True
-    #     else:
-    #         result.error = "接口返回码是 【 {} 】, 返回信息：{} ".format(res.json()["code"], res.json()["msg"])
-    #     result.msg = res.json()["msg"]
-    #     result.response = res
-    #     logger.info("注册用户 ==>> 返回结果 ==>> {}".format(result.response.text))
-    #     return result
-    #
-    # def login_user(self, username, password):
-    #     """
-    #     登录用户
-    #     :param username: 用户名
-    #     :param password: 密码
-    #     :return: 自定义的关键字返回结果 result
-    #     """
-    #     result = ResultBase()
-    #     payload = {
-    #         "username": username,
-    #         "password": password
-    #     }
-    #     header = {
-    #         "Content-Type": "application/x-www-form-urlencoded"
-    #     }
-    #     res = self.api.login(data=payload, headers=header)
-    #     if res.json()["code"] == 0:
-    #         result.success = True
-    #         result.token = res.json()["login_info"]["token"]
-    #     else:
-    #         result.error = "接口返回码是 【 {} 】, 返回信息：{} ".format(res.json()["code"], res.json()["msg"])
-    #     result.msg = res.json()["msg"]
-    #     result.response = res
-    #     logger.info("登录用户 ==>> 返回结果 ==>> {}".format(result.response.text))
-    #     return result
+    @allure.step("创建用户变量")
+    async def create_user_variable(self, variable_name: str, value: set, basic_auth: str = None) -> ResultBase:
+        result = ResultBase()
+        header = {
+            "authorization": f"Basic {basic_auth}",
+            "Content-Type": "application/json"
+        }
+        json = {
+            "value": value
+        }
+        resp = await self.api.user_variable(variable_name, headers=header, json=json, method="post")
 
+        try:
+            resp.raise_for_status()
+        except Exception as e:
+            result.msg = resp.json()["message"]
+            result.error = str(e)
+        else:
+            result.success = True
 
-if __name__ == "__main__":
-    multiprocessing.set_start_method('spawn')
-    multiprocessing.freeze_support()
-    UserOpn(utils.env.BASE_URL).get_user_info()
+        result.response = resp
+        logger.debug(f"response text {resp.text}")
+        return result
+
+    @allure.step("更新用户变量")
+    async def update_user_variable(self, old_name: str, value: str, new_name: str = None, basic_auth: str = None) -> ResultBase:
+        result = ResultBase()
+        header = {
+            "authorization": f"Basic {basic_auth}"
+        }
+        json = {
+            "value": value
+        }
+        if new_name:
+            json["name"] = new_name
+
+        resp = await self.api.user_variable(old_name, headers=header, json=json, method="PUT")
+
+        try:
+            resp.raise_for_status()
+        except Exception as e:
+            result.msg = resp.json()["message"]
+            result.error = str(e)
+        else:
+            result.success = True
+
+        result.response = resp
+        logger.debug(f"response text {resp.text}")
+        return result
+
+    @allure.step("删除当前用户创建的变量")
+    async def delete_user_variable(self, variable_name: str, basic_auth: str = None) -> ResultBase:
+        result = ResultBase()
+        header = {
+            "authorization": f"Basic {basic_auth}"
+        }
+
+        resp = await self.api.user_variable(variable_name, headers=header, method="DELETE")
+
+        try:
+            resp.raise_for_status()
+        except Exception as e:
+            result.msg = resp.json()["message"]
+            result.error = str(e)
+        else:
+            result.success = True
+
+        result.response = resp
+        logger.debug(f"response text {resp.text}")
+        return result
+
+    async def get_user_variable(self, variable_name: str, basic_auth: str = None) -> ResultBase:
+        result = ResultBase()
+        header = {
+            "authorization": f"Basic {basic_auth}"
+        }
+
+        resp = await self.api.user_variable(variable_name, headers=header, method="GET")
+
+        try:
+            resp.raise_for_status()
+            result.data = resp.json()
+        except Exception as e:
+            result.msg = resp.json()["message"]
+            result.error = str(e)
+        else:
+            result.success = True
+        result.response = resp
+        logger.debug(f"response text {resp.text}")
+        return result
